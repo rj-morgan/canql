3856e84ac06fcd4c97a806614879fa03f4c6b3c6
Tim Gentry <tim.gentry@phe.gov.uk>
2016-02-12 08:41:15 +0000

    # Trivial boyscouting

--- lib/canql/constants.rb
+++ lib/canql/constants.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This stores additional constants of the Canql gem
 module Canql
   ALL = 'all'
--- lib/canql/version.rb
+++ lib/canql/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This stores the current version of the Canql gem
 module Canql
   VERSION = '0.1.0'
