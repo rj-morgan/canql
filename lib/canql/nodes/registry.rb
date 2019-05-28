4e26ac746098452b01dac88373e401e39f309b74
Drew Thorp <drewthorp@icloud.com>
2019-05-28 14:01:40 +0100

    Add filter for processing region (Resolves #32)

--- lib/canql/nodes/registry.rb
+++ lib/canql/nodes/registry.rb
@@ -8,6 +8,17 @@ module Canql #:nodoc: all
       end
     end
 
+    module ProcessingRegistryNode
+      def meta_data_item
+        subject = reverse_scan_for_marker(:subject) == 'mother' ? '.mother' : ''
+        {
+          "unprocessed_records#{subject}.processing_registry" => {
+            Canql::EQUALS => registry.to_registrycode
+          }
+        }
+      end
+    end
+
     module RegistryCodeNode
       def to_registrycode
         text_value.upcase
